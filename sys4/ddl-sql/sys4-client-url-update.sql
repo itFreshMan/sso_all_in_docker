0f9b5ecd5356fe6e62e8032ed937f2e040e5f08f
蒋怀双 <jianghuaishuang@tydic.com>
2020-10-19 16:06:52 +0800

    初始化sql

--- sys4/ddl-sql/sys4-client-url-update.sql
+++
@@ -1,2 +0,0 @@
--- sys4系统管理 访问地址(nginx转发): ip + 端口
-update sys_client a set a.outter_url='http://192.168.129.42:30080/web-sys4' where a.CLIENT_ID='2135E3D00337471783D6AF46BD43CE4B';
--- sys4/init-sql/create-user.sql
+++ sys4/init-sql/create-user.sql
@@ -9,8 +9,10 @@ source /opt/sql/sys3.sql;
 source /opt/sql/sys3.sql;
 source /opt/sql/sys3-data-init.sql;
 source /opt/sql/sys4-patch.sql;
-source /opt/sql/sys4-client-url-update.sql;
-
 -- 数据存在中文乱码问题，改为手动执行
 -- docker-compose exec my_mariadb sh -c 'exec mysql -uroot -p"$MYSQL_ROOT_PASSWORD" sys3_db < /opt/sql/sys3-data-init.sql'
 -- docker-compose exec my_mariadb sh -c 'exec mysql -uroot -p"$MYSQL_ROOT_PASSWORD" sys3_db < /opt/sql/sys4-patch.sql'
+
+
+-- source /opt/sql/sys4-client-url-update.sql;
+-- update sys_client a set a.outter_url='http://192.168.129.42:30080/web-sys4' where a.CLIENT_ID='2135E3D00337471783D6AF46BD43CE4B';
