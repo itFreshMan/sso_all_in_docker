b7c8572e58eb03dcdb602aa2403b342d5da5794b
蒋怀双 <jianghuaishuang@tydic.com>
2020-10-22 18:33:16 +0800

    dev分支

--- setup.sh
+++ setup.sh
@@ -2,16 +2,16 @@
 docker-compose up -d
 
 ## 确保my_mariadb服务已经创建完毕， 可以在服务sys4启动完成之后，执行 初始化脚本
-service_id_cmd="docker-compose ps -q sys4"
-service_id=`eval $service_id_cmd`
-echo "`date`-----  $service_id"
-
-while [ -z $service_id ]
-do
-        echo "`date`-----  $service_id"
-        sleep 3
-        service_id=`eval $service_id_cmd`
-done
-
-## 初始化数据脚本
-docker-compose exec my_mariadb sh -c 'exec mysql -uroot -p"$MYSQL_ROOT_PASSWORD" sys3_db -e "$SSO_UPDATE_SYS4_INNER_URL"'
+#service_id_cmd="docker-compose ps -q sys4"
+#service_id=`eval $service_id_cmd`
+#echo "`date`-----  $service_id"
+#
+#while [ -z $service_id ]
+#do
+#        echo "`date`-----  $service_id"
+#        sleep 3
+#        service_id=`eval $service_id_cmd`
+#done
+#
+### 初始化数据脚本
+#docker-compose exec my_mariadb sh -c 'exec mysql -uroot -p"$MYSQL_ROOT_PASSWORD" sys3_db -e "$SSO_UPDATE_SYS4_INNER_URL"'
